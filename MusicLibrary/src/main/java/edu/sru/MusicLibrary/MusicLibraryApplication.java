7f1cecf45134ef2eb3fa4206d1659a77f68baf47
tjp1020 <tjp1020@sru.edu>
2024-01-28 20:56:53 -0500

    Setup Project

---
+++ MusicLibrary/src/main/java/edu/sru/MusicLibrary/MusicLibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.sru.MusicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryApplication.class, args);
+	}
+
+}
---
+++ MusicLibrary/src/test/java/edu/sru/MusicLibrary/MusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sru.MusicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
