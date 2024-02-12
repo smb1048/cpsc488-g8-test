221dba2350c91235ed777e600ba945b7e04b9d8f
Alexander Rihel <156694635+ajr1028@users.noreply.github.com>
2024-02-12 09:55:36 -0500

    Add files via upload

---
+++ MusicLibrary/src/main/java/edu/sru/domain/Album.java
@@ -0,0 +1,39 @@
+package edu.sru.MusicLibrary.domain;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Album {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="album")
+	private Set<Music> allMusic;
+	
+	@ManyToOne
+	private Artist artist;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
