d4daf61c035a22b3f5287ae0ca1b7840d0a7872a
Alexander Rihel <156694635+ajr1028@users.noreply.github.com>
2024-02-13 13:31:57 -0500

    Add files via upload

---
+++ MusicLibrary/src/main/java/edu/sru/MusicLibrary/controller/ExploreController.java
@@ -0,0 +1,28 @@
+package edu.sru.MusicLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.sru.MusicLibrary.repository.AlbumRepository;
+import edu.sru.MusicLibrary.repository.MusicRepository;
+@Controller
+public class ExploreController {
+	
+	private AlbumRepository albumRepository;
+	private MusicRepository musicRepository;
+	
+	public ExploreController(AlbumRepository albumRepository) {
+		this.albumRepository = albumRepository;
+	}
+	public ExploreController(MusicRepository musicRepository) {
+		this.musicRepository = musicRepository;
+	}
+	
+	@RequestMapping({"/explore"})
+    public String showAlbumList(Model model) {
+        model.addAttribute("albums", albumRepository.findAll());
+        model.addAttribute("music", musicRepository.findAll());
+        return "explore";
+    }
+}
---
+++ MusicLibrary/src/main/java/edu/sru/MusicLibrary/controller/PlaylistsController.java
@@ -0,0 +1,24 @@
+package edu.sru.MusicLibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.sru.MusicLibrary.repository.PlaylistRepository;
+
+@Controller
+public class PlaylistsController {
+	
+	private PlaylistRepository playlistRepository;
+
+	public PlaylistsController(PlaylistRepository playlistRepository) {
+		this.playlistRepository = playlistRepository;
+	}
+    
+    //Mapping for the /index URL when initiated through Tomcat
+    @RequestMapping({"/playlists"})
+    public String showPlaylistList(Model model) {
+        model.addAttribute("playlists", playlistRepository.findAll());
+        return "playlists";
+    }
+}
